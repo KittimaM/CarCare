41d54bbd3796f3d87161c1f255150aad61d5a914
toey <toey.idk@gmail.com>
2023-06-14 00:11:19 +0700

    no message

--- node-api/server.js
+++ node-api/server.js
@@ -4,8 +4,30 @@ var app = express()
 
 app.use(cors())
 
+
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password:'carcare',
+  database: 'carcare',
+  port:'3307'
+});
+
+
 app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+
+  connection.query(
+    'SELECT * FROM customer',
+    function(err, results, fields) {
+      res.json(results)
+    }
+  );
+
+  // res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
 app.listen(5000, function () {
