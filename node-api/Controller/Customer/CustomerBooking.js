6ba344bd1700b395abb4ad2ad1dada9de7673f14
riutyjku <riutyjku@gmail.com>
2023-12-08 23:35:15 +0700

    clear

--- node-api/Controller/Customer/CustomerBooking.js
+++ node-api/Controller/Customer/CustomerBooking.js
@@ -25,37 +25,6 @@ const CustomerBooking = (req, res, next) => {
     res.json({ status: "ERROR", msg: "token expired" });
   }
 
-  // jwt.verify(token, secret, function (error, decoded) {
-  //   if (error) {
-  //     res.json({ status: "ERROR", msg: error });
-  //   }
-  //   const phone = decoded.phone;
-  //   const status = "processing";
-  //   if (phone.length == 0) {
-  //     res.json({ status: "ERROR", msg: "No phone number" });
-  //   } else {
-  //     Conn.execute(
-  //       `INSERT INTO booking(car_no, car_size, service_type, service_date, payment_type, customer_user_phone, status) VALUES (?,?,?,?,?,?,?)`,
-  //       [
-  //         car_no,
-  //         car_size,
-  //         service_type,
-  //         service_date,
-  //         payment_type,
-  //         phone,
-  //         status,
-  //       ],
-  //       function (error, result) {
-  //         if (error) {
-  //           res.json({ status: "ERROR", msg: error });
-  //         } else {
-  //           const insertId = result.insertId;
-  //           res.json({ status: "SUCCESS" ,msg: insertId});
-  //         }
-  //       }
-  //     );
-  //   }
-  // });
 };
 
 module.exports = {
