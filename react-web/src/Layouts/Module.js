5884a63b6ddba734eff583ba2c3dfd7aeafcbd55
riutyjku <riutyjku@gmail.com>
2024-01-05 20:08:55 +0700

    customerbooking

--- react-web/src/Layouts/Module.js
+++ react-web/src/Layouts/Module.js
@@ -8,29 +8,9 @@ export const Button = ({ to, name }) => {
   );
 };
 
-export const TimeOptions = () => {
-  const [options, setOptions] = useState([
-    "08:00",
-    "08:30",
-    "09:00",
-    "09:30",
-    "10:00",
-    "10:30",
-    "11:00",
-    "11:30",
-  ]);
-
-  const handleSubmitSelectedTime = (event) => {
-    event.preventDefault();
-    console.log("time : ", event.target.value);
-  };
-  return (
-    <div>
-      {options.map((item) => (
-        <button onClick={handleSubmitSelectedTime} value={item}>
-          {item}
-        </button>
-      ))}
-    </div>
-  );
+export const TimeFormat = (time) => {
+  const [hour, mins] = time
+    .toLocaleTimeString("th-TH", { hour12: false })
+    .split(":");
+  return `${hour}:${mins}`;
 };
