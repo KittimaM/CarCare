2bfbb11e2413e9cb9e06fdc6d98c3c427eada1ae
beyoubyou <73053956+beyoubyou@users.noreply.github.com>
2024-05-20 23:17:27 +0700

    create master table

--- react-web/src/Layouts/Admin/SidebarAdmin.js
+++ react-web/src/Layouts/Admin/SidebarAdmin.js
@@ -1,10 +1,4 @@
-import { Modal } from 'flowbite';
-import { ModalOptions, ModalInterface } from 'flowbite';
-import { InstanceOptions } from 'flowbite';
-
-
 import React, { useEffect, useState } from "react";
-import { Button } from "../Module";
 import { GetPermission } from "../Api";
 
 
@@ -184,18 +178,6 @@ function SidebarAdmin() {
                     </a>
                 </li>
             )}
-            {permission && permission["have_payment_type_access"].includes("1") && (
-                <li>
-                    <a href="/admin/paymenttype" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-                    </svg>
-                    <span class="ms-3">Payment Type</span>
-
-                    </a>
-                </li>
-            )}
             {permission && permission["have_on_leave_personal_access"].includes("1") && (
                 <li>
                     <a href="/admin/onleave/personal" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
@@ -217,18 +199,6 @@ function SidebarAdmin() {
                     </svg>
                     <span class="ms-3">On Leave List</span>
 
-                    </a>
-                </li>
-            )}
-            
-            {permission && permission["have_on_leave_type_access"].includes("1") && (
-                <li>
-                    <a href="/admin/onleave-type" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-                    </svg>
-                    <span class="ms-3">On Leave Type</span>
                     </a>
                 </li>
             )}
@@ -256,8 +226,18 @@ function SidebarAdmin() {
                     </a>
                 </li>
             )}
-                           
-               
+            {permission && permission["have_master_table_access"].includes("1") && (
+                <li>
+                    <a href="/admin/master-table" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                    </svg>
+                    <span class="ms-3">Master Table</span>
+
+                    </a>
+                </li>
+            )}               
             </ul>
         </div>
         </aside>
